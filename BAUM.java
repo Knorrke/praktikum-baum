3b96fbdcebf9294be083d41beb25fb3d2e80bd0c
Melanie Metzger <mele-metzger@web.de>
2020-03-31 11:08:19 +0200

    ???

--- BAUM.java
+++ BAUM.java
@@ -1,15 +1,17 @@
 class BAUM
 {
     BAUMELEMENT wurzel;
-    
+
     BAUM()
     {
         wurzel = new ABSCHLUSS();
     }
-    
+
     public DATENELEMENT suchen(DATENELEMENT suchelement){
         return wurzel.Suchen(suchelement);
     }
-    
-    //public void Einfügen (DATENELEMENT neueDaten)
+
+    public void Einfügen (DATENELEMENT neueDaten){
+        wurzel.Einfügen(neueDaten);
+    }
 }
