9f221e77ee7b9fa5b59ae5e674a7ccd610c95ac3
Manuel Hermann <moehermann@web.de>
2020-03-31 10:36:12 +0200

    Klasse BAUMELEMENT erstellt

---
+++ BAUMELEMENT.java
@@ -0,0 +1,23 @@
+
+/**
+ * Beschreiben Sie hier die Klasse BAUMELEMENT.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public abstract class BAUMELEMENT
+{
+   
+  
+        
+    // Konstruktor
+    public BAUMELEMENT()
+    {
+      
+    }
+    
+    public abstract DATENELEMENT Suchen(DATENELEMENT);
+    public abstract BAUMELEMENT Einfuegen(DATENELEMENT);
+    
+    
+}
