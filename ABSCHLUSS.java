f8275d296d8714ec2a21e936f043b83a51710d24
Florian Zach <flozach93@googlemail.com>
2020-03-31 10:23:46 +0200

    ABSCHLUSS angelegt

---
+++ ABSCHLUSS.java
@@ -0,0 +1,20 @@
+
+/**
+ * Beschreiben Sie hier die Klasse ABSCHLUSS.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class ABSCHLUSS extends KNOTEN
+{
+    // Attribute - ABSCHLUSS hat keine eigenen Attribute
+
+    /**
+     * Konstruktor für Objekte der Klasse ABSCHLUSS
+     */
+    public ABSCHLUSS()
+    {
+        super();
+    }
+
+}
