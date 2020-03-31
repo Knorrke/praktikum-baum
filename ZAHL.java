0021dfca9f10d422da1196551572f8ba2e9f89f3
Melanie Metzger <mele-metzger@web.de>
2020-03-31 10:23:25 +0200

    Datenelement

---
+++ ZAHL.java
@@ -0,0 +1,22 @@
+
+/**
+ * Beschreiben Sie hier die Klasse ZAHL.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class ZAHL implements DATENELEMENT
+{
+    /**
+     * Konstruktor für Objekte der Klasse ZAHL
+     */
+    ZAHL()
+    {
+
+    }
+    
+    public int Vergleichen(DATENELEMENT daten){
+        //TODO
+        return 0;
+    }
+}
